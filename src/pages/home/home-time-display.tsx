dafdbd2e8b42f3b2e3a894f07cb15886a49c737e
wangkun <kun64855@gmail.com>
2025-01-05 17:03:08 +0800

    修正时钟less

--- src/pages/home/home-time-display.tsx
+++ src/pages/home/home-time-display.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from 'antd';
 import dayjs from 'dayjs';
-import './home-time-display.less';
 
-const HomeTimeDisplay: React.FC = React.memo(() => {
-    const [currentTime, setCurrentTime] = React.useState(dayjs());
+const HomeTimeDisplay: React.FC = () => {
+    const [currentTime, setCurrentTime] = useState(dayjs());
 
-    React.useEffect(() => {
+    useEffect(() => {
         const timer = setInterval(() => {
             setCurrentTime(dayjs());
         }, 1000);
@@ -15,20 +14,28 @@ const HomeTimeDisplay: React.FC = React.memo(() => {
     }, []);
 
     return (
-        <Card className="home-time-card">
-            {/* 日期 */}
-            <div className="home-time-date">{currentTime.format('YYYY年MM月DD日 (dddd) (GMT+8)')}</div>
+        <Card className="h-[100%]">
+            <div className="flex h-[100%] flex-col items-center justify-start space-y-8 pt-6">
+                <div className="text-xl font-medium text-gray-600">{currentTime.format('YYYY年MM月DD日 (dddd) (GMT+8)')}</div>
 
-            {/* 时间 */}
-            <div className="home-time-clock">
-                <span>{currentTime.format('HH')}</span>
-                <span className="colon">:</span>
-                <span>{currentTime.format('mm')}</span>
-                <span className="colon">:</span>
-                <span>{currentTime.format('ss')}</span>
+                <div className="flex items-center justify-center">
+                    <div
+                        className="flex items-center text-7xl font-normal text-[#1890ff]"
+                        style={{
+                            fontFamily: 'Segment7, monospace',
+                            letterSpacing: '2px',
+                        }}
+                    >
+                        <span>{currentTime.format('HH')}</span>
+                        <span className="mx-2 mb-5 opacity-80">:</span>
+                        <span>{currentTime.format('mm')}</span>
+                        <span className="mx-2 mb-5 opacity-80">:</span>
+                        <span>{currentTime.format('ss')}</span>
+                    </div>
+                </div>
             </div>
         </Card>
     );
-});
+};
 
 export default HomeTimeDisplay;
