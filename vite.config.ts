c7ff4220b07416d84da0122b80d5dc18253bb158
wangkun <kun64855@gmail.com>
2025-01-10 15:27:41 +0800

    修正自动构建

--- vite.config.ts
+++ vite.config.ts
@@ -8,60 +8,64 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    esbuild: {
-        // drop: ['console', 'debugger'],
-    },
-    css: {
-        // 开css sourcemap方便找css
-        devSourcemap: true,
-    },
-    assetsInclude: ['**/*.otf', '**/*.ttf'], // 确保包含字体文件
-    plugins: [
-        react(),
+  base: './',
+  esbuild: {
+    // drop: ['console', 'debugger'],
+  },
+  css: {
+    // 开css sourcemap方便找css
+    devSourcemap: true,
+  },
+  assetsInclude: ['**/*.otf', '**/*.ttf'], // 确保包含字体文件
+  plugins: [
+    react(),
 
-        // 同步tsconfig.json的path设置alias
-        tsconfigPaths(),
-        createSvgIconsPlugin({
-            // 指定需要缓存的图标文件夹
-            iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-            // 指定symbolId格式
-            symbolId: 'icon-[dir]-[name]',
-        }),
-        visualizer({
-            open: false,
-        }),
-    ],
-    server: {
-        host: true,
-        port: 7442,
-        proxy: {
-            '/api': {
-                target: 'http://127.0.0.1:7441',
-                changeOrigin: true,
-            },
-        },
+    // 同步tsconfig.json的path设置alias
+    tsconfigPaths(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+    }),
+    visualizer({
+      open: false,
+    }),
+  ],
+  server: {
+    host: true,
+    port: 7442,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7441',
+        changeOrigin: true,
+      },
     },
-    build: {
-        target: 'esnext',
-        minify: 'terser',
-        // rollupOptions: {
-        //   output: {
-        //     manualChunks(id) {
-        //       if (id.includes('node_modules')) {
-        //         // 让每个插件都打包成独立的文件
-        //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        //       }
-        //       return null;
-        //     },
-        //   },
-        // },
-        terserOptions: {
-            compress: {
-                // 生产环境移除console
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
+  },
+  //
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('prod'),
+  },
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         // 让每个插件都打包成独立的文件
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //       return null;
+    //     },
+    //   },
+    // },
+    terserOptions: {
+      compress: {
+        // 生产环境移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
     },
+  },
 });
